d1ca123c3113862f974711ad93eb244c4594a4a1
dan424gg <wellsdanjr@gmail.com>
2022-11-21 13:52:52 -0800

    added login processing and setpreferences within device

--- TravelBlog2/app/src/main/java/LoginActivity.kt
+++ TravelBlog2/app/src/main/java/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.travelblog
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
@@ -13,9 +15,21 @@ import com.travelblog.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var preferences: BlogPreferences
 
+    //method used on initial start up of app
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //preferences is an object ref to class BlogPreferences
+        preferences = BlogPreferences(this)
+
+        //object used to check if user is already logged in
+        if (preferences.isLoggedIn()) {
+            startMainActivity()
+            finish()
+            return
+        }
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -26,6 +40,7 @@ class LoginActivity : AppCompatActivity() {
         binding.textPasswordInput.editText?.addTextChangedListener(createTextWatcher(binding.textPasswordInput))
     }
 
+    //determines what happens when login button is pressed
     private fun onLoginClicked() {
         val username: String = binding.textUsernameLayout.editText?.text.toString()
         val password: String = binding.textPasswordInput.editText?.text.toString()
@@ -36,7 +51,28 @@ class LoginActivity : AppCompatActivity() {
             binding.textPasswordInput.error = "Password must not be empty"
         } else if (username != "admin" && password != "admin") {
             showErrorDialog()
+        } else {
+            performLogin()
         }
+    }
+
+    //used to determine what system will do during login process
+    //only delays app for 2 sec then logs user in, no HTML send
+    private fun performLogin() {
+        preferences.setLoggedIn(true)
+        binding.textUsernameLayout.isEnabled = false
+        binding.textPasswordInput.isEnabled = false
+        binding.loginButton.visibility = View.INVISIBLE
+        binding.progressBar.visibility = View.VISIBLE
+
+        Handler().postDelayed({startMainActivity()
+            finish()}, 2000)
+    }
+
+    //method used to start main activity
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
     private fun showErrorDialog() {
