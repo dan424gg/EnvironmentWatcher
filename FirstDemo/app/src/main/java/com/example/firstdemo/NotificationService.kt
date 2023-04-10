eb61831e3857ebc41502528bf24c6014b4b3ac40
The-Mooth <107499599+The-Mooth@users.noreply.github.com>
2023-04-10 12:58:12 -0700

    base service class for notifications

---
+++ FirstDemo/app/src/main/java/com/example/firstdemo/NotificationService.kt
@@ -0,0 +1,35 @@
+package com.example.firstdemo
+
+import android.app.Service
+import android.content.Intent
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+
+class NotificationService : Service() {
+
+    override fun onBind(intent: Intent): IBinder? {
+        TODO("Return the communication channel to the service. - only if we decide to use onbind ")
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        val title = intent?.getStringExtra("title") ?: ""
+        val content = intent?.getStringExtra("content") ?: ""
+        val delayMins = intent?.getLongExtra("delay", 0) ?: 0L
+
+
+
+        val runner = Runnable {
+           //make notification here
+        }
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(runner,1000*delayMins) //TODO: multiply by 60 after testing
+
+        return START_STICKY
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+}
